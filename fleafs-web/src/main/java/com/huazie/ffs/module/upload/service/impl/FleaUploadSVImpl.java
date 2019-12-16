cbf43ba69d29dfbdf6d6df96998ec0ec58b4d9c9
huazie <huazie.lgh@gmail.com>
2019-12-16 08:57:26 +0800

    huazie_文件上传优化_MOD

--- fleafs-web/src/main/java/com/huazie/ffs/module/upload/service/impl/FleaUploadSVImpl.java
+++ fleafs-web/src/main/java/com/huazie/ffs/module/upload/service/impl/FleaUploadSVImpl.java
@@ -11,18 +11,15 @@ import com.huazie.frame.common.util.IOUtils;
 import com.huazie.frame.common.util.RandomCode;
 import com.huazie.frame.common.util.StringUtils;
 import com.huazie.frame.db.common.exception.ServiceException;
-import com.huazie.frame.jersey.common.data.FleaJerseyFileContext;
-import com.huazie.frame.jersey.server.filter.FleaJerseyManager;
+import com.huazie.frame.jersey.common.FleaJerseyManager;
 import org.glassfish.jersey.media.multipart.FormDataBodyPart;
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ObjectUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.InputStream;
 
 /**
  * <p> Flea上传服务实现类 </p>
@@ -66,14 +63,8 @@ public class FleaUploadSVImpl implements IFleaUploadSV {
             throw new ServiceException("ERROR-SERVICE0000000001", "上传鉴权令牌【token】");
         }
 
-        // 获取文件信息
-        FleaJerseyFileContext fleaJerseyFileContext = FleaJerseyManager.getContext().getFleaJerseyFileContext();
-        if (ObjectUtils.isEmpty(fleaJerseyFileContext) || ObjectUtils.isEmpty(fleaJerseyFileContext.getFileFormDataBodyPart())) {
-            // 无法获取上传文件，请检查
-            throw new ServiceException("", "");
-        }
+        FormDataBodyPart fileFormDataBodyPart = FleaJerseyManager.getManager().getFileFormDataBodyPart();
 
-        FormDataBodyPart fileFormDataBodyPart = fleaJerseyFileContext.getFileFormDataBodyPart();
         FormDataContentDisposition formDataContentDisposition = fileFormDataBodyPart.getFormDataContentDisposition();
         String fileName = formDataContentDisposition.getFileName();
 
